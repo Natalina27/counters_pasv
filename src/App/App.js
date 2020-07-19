e5561888e6930af04f6f6e1699957481b44def5a
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-19 17:48:50 +0200

    Initial commit

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Counter} from "../Counter";
+
+import s from './App.module.css';
+
+
+export function App() {
+  return (
+    <div className={s.wrap}>
+      <Counter x={1}/>
+      <Counter x={10}/>
+      <Counter x={+(Math.random()*10).toFixed()}/>
+    </div>
+  );
+}
+
---
+++ src/Counter/index.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+import s from './Counter.module.css';
+
+export const Counter = ({x}) => {
+    console.log('x',x);
+
+    const [count, setCount] = useState(0);
+
+    const handleMinus = () => {
+        setCount(count - x);
+    }
+
+    const handlePlus = () => {
+        setCount(count + x);
+    }
+
+    return (
+        <div className={s.counter}>
+            <button className={s.btn} onClick={handleMinus}>-{x}</button>
+            <div className={s.count}>
+                {count}
+            </div>
+            <button className={s.btn} onClick={handlePlus}>+{x}</button>
+        </div>
+    );
+};

105885dd64b47d8f381479c40083d2aa143ee612
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-19 18:24:28 +0200

    Updates

---
+++ precache-manifest.960fdc1992a18d48c49fb8d22746b878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64817a34f99cd1fbf5d88cddd554c8ae",
+    "url": "/counters_pasv/index.html"
+  },
+  {
+    "revision": "3e524179bf8862ebfc15",
+    "url": "/counters_pasv/static/css/main.5edb74f2.chunk.css"
+  },
+  {
+    "revision": "958a3d450d57e1a92540",
+    "url": "/counters_pasv/static/js/2.b06a4175.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counters_pasv/static/js/2.b06a4175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e524179bf8862ebfc15",
+    "url": "/counters_pasv/static/js/main.ddc85115.chunk.js"
+  },
+  {
+    "revision": "8d5348258e1d62ee2de2",
+    "url": "/counters_pasv/static/js/runtime-main.4b9a4505.js"
+  }
+]);
